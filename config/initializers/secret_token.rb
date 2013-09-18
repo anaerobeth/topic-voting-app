214618e3ab109f42473d8c4cae2f3ce8fc7b47ee
Beth Tenorio <betsky98@gmail.com>
2013-09-18 11:19:44 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Clinic::Application.config.secret_key_base = '94124233618fe8de8033a727dcf46a1aa5390e036fe12a0b4f39a9c6e0650cc9c89f2b595161f5494fcbb8422ce3677916f3d5d7417cc0d49a611d913d22f401'
